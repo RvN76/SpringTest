bf05542212091bac2f92e649702432bab8ad2e70
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-02-03 23:34:03 +0100

    initial spring project

---
+++ src/main/java/com/mytest/test/DoSomething.java
@@ -0,0 +1,19 @@
+package com.mytest.test;
+
+public class DoSomething {
+	String name;
+	
+	public DoSomething(){
+		
+	}
+	
+	public DoSomething(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	
+}
---
+++ src/main/java/com/mytest/test/ReceiverController.java
@@ -0,0 +1,33 @@
+package com.mytest.test;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class ReceiverController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/receiver", method = RequestMethod.POST)
+	public String home(Model model, @RequestParam("username") String username) {
+		DoSomething a = new DoSomething(username);
+		model.addAttribute("username", username );
+		return "receiver";
+	}
+	
+}
